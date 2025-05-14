4a018910ea1c8d00cc4bc8f17d7e720fc39b8a3d
75-sanket <156225470+75-sanket@users.noreply.github.com>
2025-05-14 20:17:12 +0530

    ss

---
+++ 3..py
@@ -0,0 +1,11 @@
+wheels=int(input("enter the number"))
+if wheels==2:
+    print("it is bike")
+elif wheels==3:
+    print("it is auto or tricycle")
+elif wheels==4:
+    print("it is car")
+elif wheels>=8:
+    print("it is long  vehicle")
+else:
+    print("it is unknown vehicle")
